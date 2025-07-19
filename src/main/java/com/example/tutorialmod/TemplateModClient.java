b2f8dc1c9069c3ce6b7d8c1f93b3ce4c7de783f1
余博文 <3101606982@qq.com>
2025-07-19 12:41:10 +0800

    change info

---
+++ src/main/java/com/example/tutorialmod/TemplateModClient.java
@@ -0,0 +1,11 @@
+package com.example.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
